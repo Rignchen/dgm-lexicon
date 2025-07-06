a2440a712b64753d31fa3e2ecfdb92edab1dd1c3
Rignchen <rinchendemort@gmail.com>
2025-07-06 23:13:08 +0200

    feat: redirect to 404 if word id not valid

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from '#pages/home';
 import { Word } from '#pages/word';
+import { Error404 } from './pages/error-404/error-404';
 
 export const routes: Routes = [
 	{path: '', component: Home},
 	{path: 'words/:id', component: Word},
+	{path: '404', component: Error404},
+	{path: '**', component: Error404}
 ];
--- src/app/pages/word/word.ts
+++ src/app/pages/word/word.ts
@@ -12,6 +12,7 @@ import { Router } from '@angular/router';
 })
 export class Word implements OnInit {
 	private data = inject(Data);
+	private router = inject(Router);
 	public entry?: LexiconEntry;
 
 	constructor(private route: ActivatedRoute) {}
@@ -19,8 +20,17 @@ export class Word implements OnInit {
 	ngOnInit() {
 		this.route.params.subscribe(params => {
 			const id = params['id'];
+			if (!(id && /^\d+$/.test(id))) return this.error404();
 			const filtered = this.data.lexicon.filter(entry => entry.id === parseInt(id, 10));
+			if (filtered.length === 0) return this.error404();
 			this.entry = filtered[0];
+		});
+	}
+
+	private error404() {
+		// Navigate to the 404 page without changing the URL
+		this.router.navigate(['/404'], {
+			skipLocationChange: true
 		});
 	}
 }
