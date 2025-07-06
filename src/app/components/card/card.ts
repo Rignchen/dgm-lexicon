3769518abd662551d457d2c74e64ebcad392bdf4
Rignchen <rinchendemort@gmail.com>
2025-07-06 18:52:47 +0200

    feat: display data in cards

---
+++ src/app/components/card/card.ts
@@ -0,0 +1,12 @@
+import LexiconEntry from '#types/lexicon-entry';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.html',
+  styleUrl: './card.css'
+})
+export class Card {
+	@Input({ required: true }) entry!: LexiconEntry;
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Data } from '#services/data';
 import { NgFor } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { Card } from '../../components/card/card';
 
 @Component({
   selector: 'app-home',
-  imports: [NgFor],
+  imports: [NgFor, Card],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
