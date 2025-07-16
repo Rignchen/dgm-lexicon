bb7966c4ec131bd34b689f4f1c8f1652a3c630de
Rignchen <rinchendemort@gmail.com>
2025-07-17 01:44:27 +0200

    feat(home): use fuse.js to match & order entries

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,6 +4,7 @@ import { Card } from '#components/card';
 import { Word } from '#pages/word';
 import { Router } from '@angular/router';
 import { Bubble } from '#components/bubble';
+import Fuse from 'fuse.js';
 
 @Component({
 	selector: 'app-home',
@@ -15,6 +16,7 @@ export class Home {
 	private router = inject(Router);
 
 	private data = inject(Data);
+	private fuse = new Fuse(this.data.lexicon, {keys: ['word', 'definition']});
 	public filteredData = this.data.lexicon;
 	public cardClicked = (id: number) => Word.redirect(this.router, id);
 
@@ -24,25 +26,7 @@ export class Home {
 		if (searchTerm === '') {
 			this.filteredData = this.data.lexicon;
 		} else {
-			this.filteredData = filterAndSort(
-				this.data.lexicon,
-				entry => entry.word.toLowerCase().includes(searchTerm),
-				entry => entry.definition.toLowerCase().includes(searchTerm),
-			);
+			this.filteredData = this.fuse.search(searchTerm).map(result => result.item);
 		}
 	}
-}
-
-/**
- * Filters an array on multiple filters
- * Then sorts the array, the entries that match the first filter first, then the second, etc.
- */
-function filterAndSort<T>(data: T[], ...filters: ((item: T) => boolean)[]): T[] {
-	const filteredData = [];
-	for (const filter of filters) {
-		const filtered = data.filter(filter);
-		filteredData.push(...filtered);
-		data = data.filter(item => !filtered.includes(item));
-	}
-	return filteredData;
 }
