fbdba2b20a07a9f0e981881676c824c3c1a949c6
Rignchen <rinchendemort@gmail.com>
2025-07-17 16:35:12 +0200

    build(word): get list of id for pre-rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,18 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+import { Data } from '#services/data';
+import { inject } from '@angular/core';
+
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+	{
+		path: 'words/:id',
+		renderMode: RenderMode.Prerender,
+		getPrerenderParams: async () => inject(Data).lexicon.map(word => ({
+			id: word.id.toString(),
+		})),
+	},
+	{
+		path: '**',
+		renderMode: RenderMode.Prerender,
+	}
 ];
