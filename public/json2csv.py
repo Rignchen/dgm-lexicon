82a0c3abd2648b2d88c3dc54e82f7b3faf45a787
Rignchen <rinchendemort@gmail.com>
2025-07-18 04:48:19 +0200

    refactor(json->csv): use , instead of ; to delimit tags
    I realized libre office don't write the csv with "" around ; separated entried and then treat ; separated entries the save as , separated entries resulting in changing the content in the csv

--- public/json2csv.py
+++ public/json2csv.py
@@ -23,8 +23,8 @@ tag2-key,tag2-value
 ...
 
 id,word,definition,first_time_used,tags
-lexicon1-id,lexicon1-word,lexicon1-definition,lexicon1-first_time_used,lexicon1-tags1;lexicon1-tags2,...
-lexicon2-id,lexicon2-word,lexicon2-definition,lexicon2-first_time_used,lexicon2-tags1;lexicon2-tags2,...
+lexicon1-id,lexicon1-word,lexicon1-definition,lexicon1-first_time_used,"lexicon1-tags1,lexicon1-tags2,..."
+lexicon2-id,lexicon2-word,lexicon2-definition,lexicon2-first_time_used,"lexicon2-tags1,lexicon2-tags2,..."
 """
 
 def read_json(file_path: str) -> dict:
@@ -54,7 +54,7 @@ def convert_json_to_csv(json: dict) -> list[list[str|int]]:
 		tags.append([key, value])
 	lexicon: list[list[str|int]] = [["id", "word", "definition", "first_time_used", "tags"]]
 	for entry in json["lexicon"]:
-		tags_str = ';'.join(entry["tags"])
+		tags_str = ','.join(entry["tags"])
 		lexicon.append([
 			entry["id"],
 			entry["word"],
