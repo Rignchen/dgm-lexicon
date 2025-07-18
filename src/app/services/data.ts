974c631d45a63c76f9accea387ced5cc47fb9cbb
Rignchen <rinchendemort@gmail.com>
2025-07-18 03:52:30 +0200

    feat(json->csv): strip trailing commas

--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -3,11 +3,10 @@ import LexiconEntry from '#types/lexicon-entry'
 import * as jsonData from '#public/db.json';
 import csvData from '#public/db.csv';
 
-console.log(JSON.stringify(csvData));
-
 @Injectable({
 	providedIn: 'root'
 })
 export class Data {
 	public lexicon: LexiconEntry[] = LexiconEntry.fromJsonData(jsonData);
+	public lexiconFromCsv: LexiconEntry[] = LexiconEntry.fromCsvData(csvData);
 }
--- src/types/lexicon-entry.ts
+++ src/types/lexicon-entry.ts
@@ -37,4 +37,28 @@ export default class LexiconEntry {
 		const tags = Tag.fromObject(data.tags);
 		return LexiconEntry.fromArray(data.lexicon, tags);
 	}
+
+	static fromCsvData(data: string): LexiconEntry[] {
+		/* Assuming the CSV data is structured as follows:
+		 * ```csv
+		 * tag,color
+		 * tag0,color0
+		 * tag1,color1
+		 * ...
+		 *
+		 * id,word,definition,first_time_used,tags
+		 * 0,word0,definition0,first_time_used0,tag0-0;tag0-1;...
+		 * 1,word1,definition1,first_time_used1,tag1-0;tag1-1;...
+		 * ```
+		 * There might be trailing commas that should be removed.
+		 * The first part of the CSV is for tags, then there's a blank line, then the lexicon entries. tags in the lexicon entries are separated by semicolons to have all of them in the same field.
+		 * Some fields might have commas in them, these fields are quoted. In these quoted fields there might be quotes, these are escaped with a backslash.
+		 */
+		// Strip trailing commas from each line
+		const trailingCommaRegex = /^[\s,]*|[\s,]*$/g;
+		const lines = data.split('\n').map(line => line.replace(trailingCommaRegex, ''));
+
+		console.log(JSON.stringify(lines, null, '\t'));
+		return [];
+	}
 }
