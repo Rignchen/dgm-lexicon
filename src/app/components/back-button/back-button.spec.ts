820c7245716c06570d97012c5187da4576423661
Rignchen <rinchendemort@gmail.com>
2025-07-24 01:07:38 +0200

    test(back-button): ensure to bring back

--- src/app/components/back-button/back-button.spec.ts
+++ src/app/components/back-button/back-button.spec.ts
@@ -1,23 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BackButton } from './back-button';
+import { Location } from '@angular/common';
 
 describe('BackButton', () => {
-  let component: BackButton;
-  let fixture: ComponentFixture<BackButton>;
+	let component: BackButton;
+	let fixture: ComponentFixture<BackButton>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BackButton]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [BackButton]
+		})
+		.compileComponents();
 
-    fixture = TestBed.createComponent(BackButton);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(BackButton);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should bring back to the previous page when clicked', () => {
+		const backButtonElement: HTMLElement = fixture.nativeElement.querySelector('span');
+		const location = TestBed.inject(Location);
+		spyOn(location, 'back');
+		backButtonElement.click();
+		expect(location.back).toHaveBeenCalled();
+	});
 });
