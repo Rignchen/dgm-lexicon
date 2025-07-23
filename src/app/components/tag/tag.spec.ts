4cd66057316216eaa035d2de574f689dc26c2f25
Rignchen <rinchendemort@gmail.com>
2025-07-24 01:16:03 +0200

    test(tag): is icon displayed

--- src/app/components/tag/tag.spec.ts
+++ src/app/components/tag/tag.spec.ts
@@ -4,22 +4,36 @@ import TagType from '#types/tag';
 import { Tag } from './tag';
 
 describe('Tag', () => {
-  let component: Tag;
-  let fixture: ComponentFixture<Tag>;
+	let component: Tag;
+	let fixture: ComponentFixture<Tag>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Tag]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [Tag]
+		})
+		.compileComponents();
 
-    fixture = TestBed.createComponent(Tag);
-    component = fixture.componentInstance;
-    component.tag = new TagType('test', '#FF0000');
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(Tag);
+		component = fixture.componentInstance;
+		component.tag = new TagType('test', '#FF0000');
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('with/without icon', () => {
+		it('should render with icon', () => {
+			component.icon = true;
+			fixture.detectChanges();
+			const iconElement = fixture.nativeElement.querySelector('svg');
+			expect(iconElement).toBeTruthy();
+		});
+
+		it('should render without icon by default', () => {
+			const iconElement = fixture.nativeElement.querySelector('svg');
+			expect(iconElement).toBeFalsy();
+		});
+	});
 });
