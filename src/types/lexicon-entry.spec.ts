19007182b2c851ec8eff041af8ec8620d28de3c9
Rignchen <rinchendemort@gmail.com>
2025-07-24 04:07:30 +0200

    test(lexicon entries): create from csv

--- src/types/lexicon-entry.spec.ts
+++ src/types/lexicon-entry.spec.ts
@@ -16,4 +16,47 @@ describe("LexiconEntry", () => {
 		expect(lexiconEntry.firstSeen).toEqual(new Date("2023-10-01"));
 		expect(JSON.stringify(lexiconEntry.tags)).toBe(JSON.stringify(new Set([new Tag("testTag", "#ff0000")])));
 	});
+
+	it("should get entries from string[][]", () => {
+		const tagArrays: [string, string][] = [
+			["tag0", "#ff0000"],
+			["tag1", "#00ff00"],
+		];
+		const lexiconEntriesArrays: string[][] = [
+			["0", "word0", "definition0", "2023-10-01", "tag0"],
+			["1", "word1", "definition1", "2023-10-02", "tag1"],
+		];
+
+		const newLexiconEntriesSpy = spyOn(LexiconEntry, "new");
+		const lexiconEntries = LexiconEntry.fromCsvData(tagArrays, lexiconEntriesArrays);
+
+		expect(lexiconEntries.length).toBe(2);
+		expect(newLexiconEntriesSpy).toHaveBeenCalledTimes(2);
+		expect(newLexiconEntriesSpy).toHaveBeenCalledWith(0, "word0", "definition0", "2023-10-01", [new Tag("tag0", "#ff0000")]);
+		expect(newLexiconEntriesSpy).toHaveBeenCalledWith(1, "word1", "definition1", "2023-10-02", [new Tag("tag1", "#00ff00")]);
+	});
+
+	it("should parse from CSV data while respecting quoted fields (which can contain commas, new lines, and quotes)", () => {
+		const csvData = `tag,color,,,
+tag0,#ff0000,,,
+tag1,#00ff00,,,
+,,,,
+id,word,definition,first_time_used,tags
+0,word0,"definition0, with a comma",2023-10-01,"tag0"
+1,word1,"definition1 with a new line
+here",2023-10-02,"tag1"
+2,word2,"definition2 with a quote "" inside",2023-09-30,"tag0,tag1"`;
+
+		const fromDataSpy = spyOn(LexiconEntry, "fromCsvData");
+		LexiconEntry.parseFromCsv(csvData);
+
+		expect(fromDataSpy).toHaveBeenCalledWith([
+			["tag0", "#ff0000"],
+			["tag1", "#00ff00"],
+		], [
+			["0", "word0", "definition0, with a comma", "2023-10-01", "tag0"],
+			["1", "word1", "definition1 with a new line\nhere", "2023-10-02", "tag1"],
+			["2", "word2", "definition2 with a quote \" inside", "2023-09-30", "tag0,tag1"],
+		]);
+	});
 });
