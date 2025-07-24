f54fcaa5553b788c1f7e097750dc6608e0b84a8a
Rignchen <rinchendemort@gmail.com>
2025-07-24 03:21:45 +0200

    test(md): underline

--- src/types/markdown.spec.ts
+++ src/types/markdown.spec.ts
@@ -12,4 +12,16 @@ describe('Markdown', () => {
 			expect(result).toBe('This is bold text');
 		});
 	});
+
+	describe('underline', () => {
+		const input = new Markdown('This is __underline__ text');
+		it('should convert to HTML', () => {
+			const result = input.toHtml();
+			expect(result).toBe('This is <u>underline</u> text');
+		});
+		it('should convert to plain text', () => {
+			const result = input.toString();
+			expect(result).toBe('This is underline text');
+		});
+	});
 });
