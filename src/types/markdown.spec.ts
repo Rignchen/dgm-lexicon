c549be874f02c637f8e5bdc980a31f0edad66f48
Rignchen <rinchendemort@gmail.com>
2025-07-24 03:28:12 +0200

    test(md): heading (h1-h6)

--- src/types/markdown.spec.ts
+++ src/types/markdown.spec.ts
@@ -72,4 +72,76 @@ describe('Markdown', () => {
 			expect(result).toBe('\nThis is\na code block\n');
 		});
 	});
+
+	describe('heading 1', () => {
+		const input = new Markdown('# Heading 1');
+		it('should convert to HTML', () => {
+			const result = input.toHtml();
+			expect(result).toBe('<h1>Heading 1</h1>');
+		});
+		it('should convert to plain text', () => {
+			const result = input.toString();
+			expect(result).toBe('Heading 1');
+		});
+	});
+
+	describe('heading 2', () => {
+		const input = new Markdown('## Heading 2');
+		it('should convert to HTML', () => {
+			const result = input.toHtml();
+			expect(result).toBe('<h2>Heading 2</h2>');
+		});
+		it('should convert to plain text', () => {
+			const result = input.toString();
+			expect(result).toBe('Heading 2');
+		});
+	});
+
+	describe('heading 3', () => {
+		const input = new Markdown('### Heading 3');
+		it('should convert to HTML', () => {
+			const result = input.toHtml();
+			expect(result).toBe('<h3>Heading 3</h3>');
+		});
+		it('should convert to plain text', () => {
+			const result = input.toString();
+			expect(result).toBe('Heading 3');
+		});
+	});
+
+	describe('heading 4', () => {
+		const input = new Markdown('#### Heading 4');
+		it('should convert to HTML', () => {
+			const result = input.toHtml();
+			expect(result).toBe('<h4>Heading 4</h4>');
+		});
+		it('should convert to plain text', () => {
+			const result = input.toString();
+			expect(result).toBe('Heading 4');
+		});
+	});
+
+	describe('heading 5', () => {
+		const input = new Markdown('##### Heading 5');
+		it('should convert to HTML', () => {
+			const result = input.toHtml();
+			expect(result).toBe('<h5>Heading 5</h5>');
+		});
+		it('should convert to plain text', () => {
+			const result = input.toString();
+			expect(result).toBe('Heading 5');
+		});
+	});
+
+	describe('heading 6', () => {
+		const input = new Markdown('###### Heading 6');
+		it('should convert to HTML', () => {
+			const result = input.toHtml();
+			expect(result).toBe('<h6>Heading 6</h6>');
+		});
+		it('should convert to plain text', () => {
+			const result = input.toString();
+			expect(result).toBe('Heading 6');
+		});
+	});
 });
