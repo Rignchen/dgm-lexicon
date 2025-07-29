03c9fe4cf570b2cf71887df188a97dfbd93263d5
Rignchen <rinchendemort@gmail.com>
2025-07-29 21:36:28 +0200

    feat: add image metadata for discord

--- src/app/app.ts
+++ src/app/app.ts
@@ -16,6 +16,10 @@ export class App {
 			{name: 'og:description', content: 'Un lexique des références et des termes utilisés par la communauté de devenirgamemaster'},
 			{name: 'og:title', content: 'DGM Lexicon'},
 			{name: 'og:image', content: 'https://rignchen.github.io/dgm-lexicon/qr-code.svg'},
+			{name: 'og:image:alt', content: 'QR Code vers le lexique DGM'},
+			{name: 'og:image:width', content: '600'},
+			{name: 'og:image:height', content: '600'},
+			{name: 'og:image:type', content: 'image/svg+xml'},
 		], true);
 	}
 }
