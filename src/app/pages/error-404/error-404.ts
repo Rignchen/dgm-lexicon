5e917e02fbe39a51b827fb613c571682256b486a
Rignchen <rinchendemort@gmail.com>
2025-07-29 20:38:11 +0200

    feat(404): add meta tag for discord like apps

--- src/app/pages/error-404/error-404.ts
+++ src/app/pages/error-404/error-404.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Bubble } from '#components/bubble';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-error-404',
@@ -10,8 +10,10 @@ import { Title } from '@angular/platform-browser';
 	styleUrl: './error-404.css'
 })
 export class Error404 {
-	constructor(private router: Router, private title: Title) {
+	constructor(private router: Router, private title: Title, private meta: Meta) {
 		this.title.setTitle("Page Not Found");
+		this.meta.updateTag({name: 'og:title', content: 'Page Not Found'});
+		this.meta.updateTag({name: 'og:description', content: 'Le goblin chargé de rapporter cette page a été mangé par un dragon.'});
 	}
 
 	public goHome() {
